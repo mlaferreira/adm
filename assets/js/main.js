42b9c84a3ea0460f629dbee14252e9bfbc1443c7
mlaferreiraweb <mlaferreira.marcello@gmail.com>
2023-12-30 17:55:04 -0300

    melhorando o sistema

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const openLink = document.querySelector('#criar-escala');
+const boxEscala = document.querySelector('.form-escala');
+
+openLink.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    
+    boxEscala.classList.toggle('close');    
+    
+   
+})
